abde4f3379242ce6e51d1010bc8e088b6f928a7f
Arjun Subramanyam Varalakshmi <arjunsubramanyamvaralakshmi@Arjuns-Mac-mini.local>
2022-04-03 08:38:39 -0700

    add the styled components for Header file

--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,20 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Header = (props) => {
+
+    const Wrapper = styled.div`
+        padding: 50px 100px 50px 0;
+        font-size: 30px;
+
+        img {
+            height: 60px;
+            width: 60px;
+            border-radius: 100%;
+            border: 1px solid rgba(0,0,0,0.1);
+            margin-bottom: -8px;
+        }
+
+        
+    `
+    const TotalReviews = styled.div`
+        font-size: 18px;
+        padding: 10px 0;
+    `
+    const TotalOutOf = styled.div`
+        font-size: 18px;
+        font-weight: bold;
+        padding: 10px 0;
+    `
+
 
     const { name , image_url, avg_score } = props.attributes
     const total_reviews = props.reviews.length
 
     return(
-        <div className='wrapper'>
+        <Wrapper>
             <h1> <img src={image_url} alt={name} /> {name} </h1>
              
             <div>
-                <div className='totalReviews'>{total_reviews} user reviews</div>
+                <TotalReviews>{total_reviews} user reviews</TotalReviews>
                 <div className='starReviews'></div>
-                <div className='totalOutOf'> 3 out of 5</div>
+                <TotalOutOf> 3 out of 5</TotalOutOf>
             </div>
-        </div>
+        </Wrapper>
     )
 }
 
